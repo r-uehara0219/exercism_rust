c1e8a090ba215598cb4217a8d63dc24eb8013272
r-uehara0219 <r_uehara@primenumber.co.jp>
2021-01-11 23:05:19 +0900

    gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,5 @@
-use chrono::{DateTime, Utc, Duration};
+use chrono::{DateTime, Duration, Utc};
 
-// Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    return start + Duration::seconds(1000000000)
+    return start + Duration::seconds(1_000_000_000)
 }
