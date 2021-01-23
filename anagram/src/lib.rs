9b3a2ba49eec4989a24abaf47a7cb977932061d3
r-uehara0219 <r_uehara@primenumber.co.jp>
2021-01-24 00:18:24 +0900

    anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -2,16 +2,15 @@ use std::collections::HashSet;
 
 fn sort_word(word: &str) -> String {
     let mut chars: Vec<char> = word.to_lowercase().chars().collect();
-    chars.sort();
+    chars.sort_unstable();
     return chars.into_iter().collect();
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let mut correct_anagrams = HashSet::with_capacity(6);
-    let sorted_word: String = sort_word(word);
+    let mut correct_anagrams = HashSet::with_capacity(possible_anagrams.len());
 
     for anagram in possible_anagrams.iter() {
-        if sort_word(anagram) == sorted_word && anagram != &word {
+        if sort_word(anagram) == sort_word(word) && word.to_lowercase() != anagram.to_lowercase() {
             correct_anagrams.insert(*anagram);
         }
     }
